e37af3af74f0ee9b58035edf9cf82098ace00766
likith22 <65149464+likith22@users.noreply.github.com>
2020-12-26 09:58:56 +0530

    Update KthLargestFactor.java

--- KthLargestFactor.java
+++ KthLargestFactor.java
@@ -1,3 +1,20 @@
+/*Question -: A positive integer d is said to be a factor of another positive integer N if when N is divided by d, the remainder obtained is zero. For example, for number 12, there are 6 factors 1, 2, 3, 4, 6, 12. Every positive integer k has at least two factors, 1 and the number k itself.Given two positive integers N and k, write a program to print the kth largest factor of N.
+
+Input Format: The input is a comma-separated list of positive integer pairs (N, k).
+
+Output Format: The kth highest factor of N. If N does not have k factors, the output should be 1.
+
+Constraints:
+
+1<N<10000000000
+1<k<600.
+You can assume that N will have no prime factors which are larger than 13.
+
+Example 1
+
+Input: 12,3
+Output: 4
+Explanation: N is 12, k is 3. The factors of 12 are (1,2,3,4,6,12). The highest factor is 12 and the third largest factor is 4. The output must be 4.*/
 package tcsCodeVita;
 import java.util.Scanner;
 
